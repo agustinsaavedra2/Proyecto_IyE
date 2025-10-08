0d8c738df13298b1c6328bef38aeaa8b316e0715
Ignacio Celis Castro <ignacio.celis@usach.cl>
2025-10-08 12:01:20 -0300

    Creación del backend

---
+++ BackendIE/src/main/java/com/BackendIE/BackendIE/BackendIeApplication.java
@@ -0,0 +1,13 @@
+package com.BackendIE.BackendIE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendIeApplication.class, args);
+	}
+
+}
